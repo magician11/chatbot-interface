49a181bedc27f9ffb7ba36ce55342dc63d6ef5d4
Andrew Golightly <support@andrewgolightly.com>
2017-11-03 00:00:46 +0000

    added connect to wit.ai

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./Chatroom.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Chatroom.css';
+import Message from './Message.js';
 
-import Message from "./Message.js";
+const config = require('./config.json');
 
 class Chatroom extends React.Component {
   constructor(props) {
@@ -11,12 +12,8 @@ class Chatroom extends React.Component {
     this.state = {
       chats: [
         {
-          entity: "bot",
-          content: "Why hello there!"
-        },
-        {
-          entity: "bot",
-          content: "What can I do for you today?"
+          entity: 'bot',
+          content: 'Why hello there!'
         }
       ]
     };
@@ -38,30 +35,66 @@ class Chatroom extends React.Component {
     ).scrollHeight;
   }
 
-  submitMessage(e) {
+  getBotResponse = text => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let response = await fetch(`${config.url}${text}`);
+        let responseJson = await response.json();
+        resolve(responseJson);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  // submit message and receive a response
+  submitMessage = async e => {
     e.preventDefault();
+
+    const userEnteredText = ReactDOM.findDOMNode(this.refs.msg).value;
 
     this.setState(
       {
         chats: this.state.chats.concat([
           {
-            username: "human",
-            content: ReactDOM.findDOMNode(this.refs.msg).value
+            entity: 'human',
+            content: userEnteredText
           }
         ])
       },
       () => {
-        ReactDOM.findDOMNode(this.refs.msg).value = "";
+        ReactDOM.findDOMNode(this.refs.msg).value = '';
       }
     );
-  }
+
+    const botResponse = await this.getBotResponse(userEnteredText);
+    console.log(botResponse);
+    let responseToUser;
+    if (botResponse.entities.intent) {
+      const intention = botResponse.entities.intent[0];
+      responseToUser = `Looks like your intention was "${intention.value}". I'm ${Math.round(
+        intention.confidence * 100
+      )}% sure of this.`;
+    } else {
+      responseToUser = "Mmmm... not sure what you're trying to say here...";
+    }
+
+    this.setState({
+      chats: this.state.chats.concat([
+        {
+          entity: 'bot',
+          content: responseToUser
+        }
+      ])
+    });
+  };
 
   render() {
     const { chats } = this.state;
 
     return (
       <div className="chatroom">
-        <h1>Go For Self</h1>
+        <h1>{config.botName}</h1>
         <ul className="chats" ref="chats">
           {chats.map((chat, i) => <Message chat={chat} key={i} />)}
         </ul>
